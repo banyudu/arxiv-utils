60640d03283fc262314f943ea15ce29c86a2c884
banyudu <banyudu@gmail.com>
2025-02-08 17:52:50 +0800

    feat: download arxiv pdf upon action button click

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,6 +1,40 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+browser.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+    const activeTab = (await browser.tabs.query({ active: true, currentWindow: true }))?.[0];
+    console.log("Received request in bg: ", request);
+    return Promise.resolve({ test: activeTab.id });
 
     if (request.greeting === "hello")
         return Promise.resolve({ farewell: "goodbye" });
+});
+
+browser.action.onClicked.addListener(async (request, sender, sendResponse) => {
+    const activeTab = (await browser.tabs.query({ active: true, currentWindow: true }))?.[0];
+    if (!activeTab) {
+        return;
+    }
+    
+    const url = activeTab.url;
+
+    browser.tabs.sendMessage(activeTab.id, { greeting: "hello1", url });
+    
+    if (url?.startsWith("https://arxiv.org/abs/")) {
+        const paperId = url.split("/").pop().replace(/[?#].*$/, '');
+        const title = activeTab.title;
+        const pdfUrl = `https://arxiv.org/pdf/${paperId}`;
+        const filename = `${title}.pdf`;
+        browser.tabs.sendMessage(activeTab.id, { action: "download", url: pdfUrl, filename });
+        
+        // Safari doesn't support downloads API
+//        try {
+//            browser.tabs.sendMessage(activeTab.id, { greeting: "download-options", pdfUrl, filename });
+//            browser.tabs.sendMessage(activeTab.id, { greeting: "debug", type: typeof browser.downloads });
+//            const downloadId = await browser.downloads.download({ url: pdfUrl, filename: 'a.pdf', saveAs: true });
+//            console.log("Download id: ", downloadId);
+//            browser.tabs.sendMessage(activeTab.id, { greeting: "downloadId", downloadId });
+//        } catch (error) {
+//            console.error("Download error: ", error);
+//            browser.tabs.sendMessage(activeTab.id, { greeting: "downloadError", error: JSON.stringify(error) });
+//        }
+//        await browser.downloads.download({ url: pdfUrl, filename });
+    }
 });
--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,37 @@
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+//    console.log("Received response in cs: ", response);
 });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+//    console.log("Received request in cs: ", request);
+    if (request.action === "download") {
+        const url = request.url;
+        const filename = request.filename;
+//        const link = document.createElement('a');
+//        link.href = url;
+//        link.download = filename;
+//        link.target = "_blank";
+//        link.click();
+//        const pdf = await fetch(url).then(response => response.blob());
+//        const file = new File([pdf], filename, { type: "application/pdf" });
+//        navigator.share({ files: [file] });
+        // Fetch the file data
+        fetch(url)
+            .then(response => response.blob())
+            .then(blob => {
+                // Create an object URL for the blob
+                const url = URL.createObjectURL(blob);
+                
+                // Create an anchor element and trigger a download
+                const link  = document.createElement("a");
+                link.href = url;
+                link.download = filename;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            });
+    }
 });
+
+console.log("injection arxiv-utils script on " + document.location.href);
+
