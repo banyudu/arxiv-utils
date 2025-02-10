4cb98f73c1c5dbc872e05247e91005e7b9febb7a
banyudu <banyudu@gmail.com>
2025-02-10 17:32:01 +0800

    feat: add download anchor to the search and detail page

---
+++ README.md
@@ -0,0 +1,8 @@
+# Arxiv Utils for Safari
+
+This is a Safari extension which can be used to download the PDFs of papers from Arxiv.
+
+Features:
+
+- Download PDFs of papers from Arxiv instead of opening them in the browser.
+- Download PDF of a single paper by clicking on the extension icon in the toolbar.
--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,3 +1,11 @@
+function getPaperIdFromUrl(url) {
+  return url.split("/").pop().replace(/[?#].*$/, '');
+}
+
+function formatFilename(paperId, title) {
+  return `[${paperId}] ${title}.pdf`;
+}
+
 function download(url, filename) {
   fetch(url)
     .then(response => response.blob())
@@ -10,6 +18,19 @@ function download(url, filename) {
       link.click();
       document.body.removeChild(link);
     })
+}
+
+function buildDownloadAnchor(url, filename) {
+  const downloadAnchor = document.createElement("a");
+  downloadAnchor.href = "#";
+  // download emoji
+  downloadAnchor.innerText = " ⬇️";
+  downloadAnchor.onclick = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    download(url, filename);
+  }
+  return downloadAnchor
 }
 
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
@@ -25,28 +46,44 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
-
-// https://arxiv.org/list/cs.AI/recent
-// add quick download button to the list page
+// add quick download buttons
 if (document.location.href.startsWith("https://arxiv.org/list")) {
+  // https://arxiv.org/list/cs.AI/recent
   const pdfLinks = Array.from(document.querySelectorAll("a[title='Download PDF']"));
   pdfLinks.forEach(link => {
     const url = link.href;
-    const paperId = link.href.split("/").pop();
+    const paperId = getPaperIdFromUrl(link.href);
     const titleNode = link.parentElement?.nextElementSibling?.querySelectorAll("div.list-title")[0];
     const title = titleNode.innerText?.trim();
-    const filename = `[${paperId}] ${title}.pdf`
+    const filename = formatFilename(paperId, title);
 
-    const downloadAnchor = document.createElement("a");
-    downloadAnchor.href = "#";
-    // download emoji
-    downloadAnchor.innerText = " ⬇️";
-    // downloadAnchor.innerText = "";
-    downloadAnchor.onclick = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      download(url, filename);
-    }
+    const downloadAnchor = buildDownloadAnchor(url, filename);
+    link.appendChild(downloadAnchor);
+  });
+} else if (document.location.href.startsWith("https://arxiv.org/abs/")) {
+  // https://arxiv.org/abs/2501.09166
+  const link = document.querySelector("a.download-pdf");
+  const url = link?.href;
+  const title = document.title;
+  const filename = `${title}.pdf`
+
+  const downloadAnchor = buildDownloadAnchor(url, filename);
+  link.appendChild(downloadAnchor);
+  link.appendChild(downloadAnchor);
+} else if (document.location.href.startsWith("https://arxiv.org/search")) {
+  // https://arxiv.org/search/?query=attention&searchtype=all&abstracts=show&order=-announced_date_first&size=50
+  const pdfLinks = Array.from(document.querySelectorAll("li.arxiv-result .list-title a")).filter(link => link.innerText === "pdf");
+  pdfLinks.forEach(link => {
+    const url = link.href;
+    const paperId = getPaperIdFromUrl(link.href);
+
+    const arxivResult = link.closest("li.arxiv-result");
+    const titleNode = arxivResult.querySelector("p.title");
+
+    const title = titleNode.innerText?.trim();
+    const filename = formatFilename(paperId, title);
+
+    const downloadAnchor = buildDownloadAnchor(url, filename);
     link.appendChild(downloadAnchor);
   });
 }
